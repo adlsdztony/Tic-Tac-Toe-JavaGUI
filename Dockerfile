920d2c6e36f3e88bc1799627ebd4fc3ee318ed39
adlsdztony <zzl0712@outlook.com>
2023-11-25 14:16:19 +0800

    Add Dockerfile and update Main.java

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM openjdk:8-jdk-alpine AS build
+# copy source code
+COPY . /usr/src/app
+# set working directory
+WORKDIR /usr/src/app
+# build jar
+RUN javac -d classes .\src\ass4\*
+RUN jar --create --file app.jar --manifest .\MANIFEST.MF -C classes .
+
+
+# Path: Dockerfile
+FROM openjdk:8-jre-alpine
+# copy jar from build stage
+COPY --from=build /usr/src/app/app.jar /usr/app/app.jar
+# set working directory
+WORKDIR /usr/app
+# run jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
+
+
--- src/ass4/Main.java
+++ src/ass4/Main.java
@@ -1,22 +1,51 @@
 package ass4;
 
+import java.net.ServerSocket;
+
 import javax.swing.SwingUtilities;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		String ip;
+		String mode;
 		if (args.length == 0) {
 			ip = "localhost";
-		} else {
-			ip = args[0];
-		}
-        SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				View view = new View();
-				Controller controller = new Controller(view, ip);
-				controller.start();
+			mode = "client";
+		} else if (args.length == 1) {
+			if (args[0].equals("server")) {
+				ip = "localhost";
+				mode = "server";
+			} else {
+				ip = args[0];
+				mode = "client";
 			}
-		});
-    }
+		} else {
+			ip = args[1];
+			mode = args[0];
+		}
+		if (mode.equals("client")) {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					View view = new View();
+					Controller controller = new Controller(view, ip);
+					controller.start();
+				}
+			});
+		} else if (mode.equals("server")) {
+			System.out.println("Server is Running...");
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				public void run() {
+					System.out.println("Server Stopped.");
+				}
+			}));
+
+			try (var listener = new ServerSocket(12396)) {
+				Server myServer = new Server(listener);
+				myServer.start();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 }
